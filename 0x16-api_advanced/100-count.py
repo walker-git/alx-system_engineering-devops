f9cae4041706f73ecb2e33e508315219e66a16ef
walker-git <setiks92@gmail.com>
2023-05-09 22:25:23 -0700

    API Advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
-"""
-100-main
-"""
-import sys
+import requests
+from collections import Counter
 
-if __name__ == '__main__':
-    count_words = __import__('100-count').count_words
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+def count_words(subreddit, word_list, after=None, counts=None):
+    if counts is None:
+        counts = Counter()
+    if after == '':
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print(f"{word}: {count}")
+        return
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": "100", "after": after}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            title = post['data']['title'].lower()
+            for word in word_list:
+                if f" {word.lower()} " in f" {title} ":
+                    counts[word.lower()] += 1
+        after = data['data']['after']
+        count_words(subreddit, word_list, after, counts)
     else:
-        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
+        print(f"Error: {response.status_code}")
+
+
