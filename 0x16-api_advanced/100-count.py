548a8f17d0c1d2b6695d4a46207837a3ddf1857d
walker-git <setiks92@gmail.com>
2023-05-09 22:51:29 -0700

    API Advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,66 +1,43 @@
 #!/usr/bin/python3
+"""Contains the count_words function"""
 import requests
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    """
-    Recursively retrieves hot posts in a subreddit, counts occurrences of
-    keywords in the titles of those posts, and prints a sorted count of the
-    keywords.
+def count_words(subreddit, word_list, found_list=[], after=None):
+    '''Prints counts of given words found in hot posts of a given subreddit.
 
-    subreddit (str): The name of the subreddit to search.
-    word_list (List[str]): A list of keywords to search for.
-    after (str): The value to pass as the "after" parameter in the Reddit API
-        request, for pagination.
-    counts (Dict[str, int]): A dictionary mapping keywords to their counts so far.
-    """
-    # If this is the first call to the function, initialize the counts dict
-    if counts is None:
-        counts = {}
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        found_list (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+    '''
+    user_agent = {'User-agent': 'test45'}
+    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                         .format(subreddit, after), headers=user_agent)
+    if after is None:
+        word_list = [word.lower() for word in word_list]
 
-    # If we've already counted all the words, return the final counts
-    if not word_list:
-        return counts
-
-    # Get the first word in the list and remove it from the list
-    word = word_list[0]
-    word_list = word_list[1:]
-
-    # If the word is empty or contains invalid characters, skip it
-    if not word or not word.isalnum():
-        return count_words(subreddit, word_list, after=after, counts=counts)
-
-    # Make the API request to Reddit to get the hot posts in the subreddit
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 100}
-    if after:
-        params['after'] = after
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    # If the subreddit is invalid or the request was unsuccessful, exit
-    if response.status_code != 200:
+    if posts.status_code == 200:
+        posts = posts.json()['data']
+        aft = posts['after']
+        posts = posts['children']
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    found_list.append(word)
+        if aft is not None:
+            count_words(subreddit, word_list, found_list, aft)
+        else:
+            result = {}
+            for word in found_list:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda item: item[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
+    else:
         return
-
-    # Parse the JSON response
-    data = response.json()
-
-    # If there are no posts left to process, return the final counts
-    if not data['data']['children']:
-        return counts
-
-    # Loop through the posts and count occurrences of the word in the titles
-    for post in data['data']['children']:
-        title = post['data']['title'].lower()
-        if word.lower() in title.split():
-            if word in counts:
-                counts[word] += 1
-            else:
-                counts[word] = 1
-
-    # Recursively call the function with the remaining words and the "after" parameter
-    return count_words(subreddit, word_list, after=data['data']['after'], counts=counts)
-
-
-# Example usage: count_words('programming', ['react', 'python', 'java', 'javascript', 'scala', 'no_results_for_this_one'])
-
